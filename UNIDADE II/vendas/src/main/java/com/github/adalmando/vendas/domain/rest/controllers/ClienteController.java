5b5b783e7370f5d230361a89d3f03d503d89d57b
Adalmando da Silva Araujo <adalmando.silva@labs.dcx>
2023-02-09 11:06:38 -0300

    nada

--- UNIDADE II/vendas/src/main/java/com/github/adalmando/vendas/domain/rest/controllers/ClienteController.java
+++ UNIDADE II/vendas/src/main/java/com/github/adalmando/vendas/domain/rest/controllers/ClienteController.java
@@ -1,35 +1,14 @@
 package com.github.adalmando.vendas.domain.rest.controllers;
 
-import com.github.adalmando.vendas.domain.entity.Cliente;
-import com.github.adalmando.vendas.domain.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
-    private ClienteRepository clienteRepository;
-    public ClienteController(ClienteRepository clienteRepository){
-        this.clienteRepository = clienteRepository;
-    }
-
-    @GetMapping("/api/clientes/{id}")
-    @ResponseBody
-    public ResponseEntity getClienteById(@PathVariable Integer id) // com o pathvariable eu digo que o {id}
-    {                                                                       // da URL é o mesmo id dos parametros passados.
-        Optional<Cliente> cliente = clienteRepository.findById(id);
-
-        if(cliente.isPresent()) {
-            return ResponseEntity.ok(cliente.get() );
-        }
-        return ResponseEntity.notFound().build();
-    }
-
-    public ResponseEntity save(Cliente cliente){
-        
+    @RequestMapping(value = "/")
+    public String helloClientes(String nomeCliente){
+        return String.format("Hello %s", nomeCliente);
     }
 }
